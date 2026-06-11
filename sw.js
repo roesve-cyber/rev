6937c7cb24f0a4175a208a4711a465fc23abb68c
roesve-cyber <roesve@gmail.com>
2026-06-11 17:12:22 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ try {
   console.warn('No se pudieron cargar scripts de Firebase Messaging:', err);
 }
 
-var CACHE_NAME = 'mmp-pos-v37';
+var CACHE_NAME = 'mmp-pos-v38';
 var ASSETS = [
   '/',
   '/index.html',
