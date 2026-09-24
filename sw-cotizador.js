4426e7427b56c652a1b1afffcc32fa88c5ce0627
roesve-cyber <roesve@gmail.com>
2026-09-23 18:00:00 -0600

    Add files via upload

--- sw-cotizador.js
+++ sw-cotizador.js
@@ -4,7 +4,7 @@
 // SIEMPRE va a la red por los datos reales (precios, tasas, guardado de
 // cotizaciones). Nunca cachea ni intercepta nada de Firebase/Firestore/Google.
 
-const CACHE_NAME = 'cotizador-shell-v6';
+const CACHE_NAME = 'cotizador-shell-v7';
 
 const SHELL_URLS = [
   '/cotizador-movil.html',
